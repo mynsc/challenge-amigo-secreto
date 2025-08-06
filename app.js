06feaae45d1b4fd7fff3adea8d1d6f6d070f6ef4
mynsc <mcerquin.10@gmail.com>
2025-08-05 22:50:13 -0500

    Add añadirAmigo function

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaDeAmigos = [];
+
+function agregarAmigo() {
+    // Enlace entre el input el archivo html y el archivo js por DOM
+    let  = document.getElementById('amigo');
+    // Acceso al contenido del input
+    let amigoAgregado = nombre.value;
+
+    // Verifica si la entrada no está vacía
+    if (amigoAgregado != '') {
+        listaDeAmigos.push(amigoAgregado);
+        nombre.value = ''; // Limpia la entrada luego de añadir el nombre de un amigo
+    }
+    else {
+         alert('Por favor, inserte un nombre');
+    }
+
+    console.log(listaDeAmigos);
+}
